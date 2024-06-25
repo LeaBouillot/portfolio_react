0f48a72a72f26a949ad958c874454cda3855735a
bestdevelope <leabouillot7@gmail.com>
2024-06-25 16:50:20 +0200

    react-router-dom : NavLink

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import'./main.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//Create ROUTER
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/skills",
+    element: <div>Skills</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </React.StrictMode>,
 )
