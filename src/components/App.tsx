1c1a8076eac3c6cc5cc5905a67291555dae9f1ae
bestdevelope <leabouillot7@gmail.com>
2024-06-26 09:31:38 +0200

    app

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Nav from "./components/Nav.tsx";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <h1>Accueil</h1>
+    </>
+  );
+}
+
+export default App;
